1fc1d6d57d25b5fd7e2b7145c537c167bca974b1
Luiz Otavio <luizotaviomellosouza@gmail.com>
2023-08-21 11:14:06 -0300

    atividade_impreção_de_numeros
    Precisamos imprimir um número para cada andar de um hotel de 20 andares. Porém, o dono do hotel é supersticioso e optou por não ter um 13ro andar.

---
+++ imprecao_de_numero.py
@@ -0,0 +1,12 @@
+# primeira forma de resolver o resolver o problemas usando o laço "for" 
+for i in range(20, 0, -1):
+    if (i != 13):
+     print('andar ' + str(i))
+#segunda forma  é utilizando o laço while 
+l=20
+print("Segunda Forma")
+while(l>=0):
+  if (l != 13):
+   print("andar " + str(l) )
+  l=l-1
+
